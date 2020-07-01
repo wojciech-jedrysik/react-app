f2c08c33620c2ad77d54e6f14b79660fe13e1217
wojciech-jedrysik <48248889+wojciech-jedrysik@users.noreply.github.com>
2020-07-01 19:10:19 +0200

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from "react-redux";
+import './index.css';
+import App from './App';
+import store from "./store/store";
+import { BrowserRouter } from 'react-router-dom';
+
+ReactDOM.render(
+    <Provider store={store}>
+        <BrowserRouter><App /></BrowserRouter>
+    </Provider>
+    , document.getElementById('root'));
